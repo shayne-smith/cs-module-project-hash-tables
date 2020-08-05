2f1d41d9b4e40c4116ac0d546f365f69509f3873
seanmx96 <seanmx96@users.noreply.github.com>
2020-08-05 13:41:58 -0500

    Implemented lookup_table with memoization

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,5 +1,7 @@
-# Your code here
+import random
+import math
 
+d = {}
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -14,7 +16,17 @@ def slowfun(x, y):
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
+    if (x, y) in d:
+        return d[(x, y)]
+
+    v = math.pow(x, y)
+    v = math.factorial(v)
+    v //= (x + y)
+    v %= 982451653
+    
+    d[(x, y)] = v
+
+    return v
 
 
 
